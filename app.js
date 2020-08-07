5f08b4b10e8b5d5345c5e06356cfb69af4086558
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-08-07 17:01:18 +0300

    azureappservice

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 var path = require('path');
@@ -25,7 +26,7 @@ app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist
  
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  //app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
